4b00cdc0f39ba6a98b21e84f82ec3bc82d4796b9
sargun <sargun.narula5@gmail.com>
2021-10-20 10:43:57 +0530

    #Fix- Add google login and client code

---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+import {BrowserRouter as Router,Route,Switch,Redirect} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import Home from './components/home.js'
+
+
+
+class App extends Component {
+/*   constructor(){
+    super();
+
+    this.state={isLogin:true};
+
+    this.state={isLogin:false};
+
+  }
+  componentDidMount(){
+    const key=localStorage.getItem("userKey");
+    if(key==null){
+     this.setState({
+       isLogin:true
+     });
+    }
+    else{
+      this.setState({
+        isLogin:true
+      });
+    }
+    console.log(this.state.isLogin);
+  } */
+
+
+  render(){
+    
+    const history = createBrowserHistory();
+    return (
+      
+      <React.Fragment>
+        <Router history={history}>
+         <Switch>
+            
+            <Route path="/" exact component={Home}    />
+
+            {/* <Route path="/About" exact component={About}    />
+            
+            <Route path="/Algorithms" exact >
+               {this.state.isLogin ?  <Algohome />: <Redirect to="/" /> } 
+            </Route>  
+
+            
+            <Route path="/Algorithms/:name" exact>
+                {this.state.isLogin ?  <Algopage />: <Redirect to="/" /> }
+            </Route>
+
+            <Route path="/DataStructures" exact >
+               {this.state.isLogin ?  <DShome />: <Redirect to="/" /> } 
+            </Route>  
+
+            
+            <Route path="/DataStructures/:name" exact>
+            {this.state.isLogin ?  <DSpage />: <Redirect to="/" /> }
+            </Route> */}
+
+        </Switch> 
+       </Router>        
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
